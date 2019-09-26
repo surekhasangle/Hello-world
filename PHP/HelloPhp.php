3086d52d6bdca23e857f17fc54344964ff5e9242
surekhasangle <44473965+surekhasangle@users.noreply.github.com>
2019-09-26 13:25:56 +0530

    Update HelloPhp.php

--- PHP/HelloPhp.php
+++ PHP/HelloPhp.php
@@ -1,9 +1,10 @@
 <html>
  <head>
   <title>Hello World</title>
- </head>
+</head>
  <body>
   <marquee><h1>Hello World</h1></marquee>
  <?php echo "<center><h3>Hello Php!</h3></center>"; ?>
+  <?php echo "my name is php"; ?>
  </body>
 </html>
